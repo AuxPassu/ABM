9bfbc8db221b34a7bcd3e1b71c42816e8b514afa
AuxPassu <50345929+AuxPassu@users.noreply.github.com>
2019-05-06 21:55:05 -0300

    Add files via upload

---
+++ Menu.h
@@ -0,0 +1,17 @@
+/** \brief Limpia la pantalla, muestra un menu de opciones en base al parametro recibido
+           y devuelve un entero que representa la opcion elegida por el usuario.
+ *
+ * \param char[] Las opciones a mostrar.
+ * \return int Opcion elegida por el usuario.
+ *
+ */
+int showMenu(char[]);
+
+/** \brief Funcion principal donde se realizan operaciones de acuerdo a la opcion
+           elegida por el usuario.
+ *
+ * \param void No recibe parametros.
+ * \return void No devuelve algo.
+ *
+ */
+void chooseOption(void);
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Menu.h"
+#include "ArrayEmployees.h"
+#include "Inputs.h"
+#include "Utilities.h"
+#include "stringFormat.h"
+
+int main()
+{
+    chooseOption();
+}
